302b3e200895fc028be1c80d7a373c41cd3fef5a
Reece Metcalfe <127119899+rmetcalfe-msp@users.noreply.github.com>
2024-06-26 22:53:00 +0100

    Updated to defaultdict

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import datetime
 import streamlit as st
 import pandas as pd
@@ -5,43 +6,42 @@ import json
 
 from pathlib import Path
 
+from traitlets import default
+
 BASE_DIR = Path(__file__).resolve().parent
-ENCODING = 'utf-8'
+ENCODING = "utf-8"
 
 # Load data
-with open(BASE_DIR / 'euro.json', encoding=ENCODING) as f:
+with open(BASE_DIR / "euro.json", encoding=ENCODING) as f:
     euro_data = json.load(f)
 
-with open(BASE_DIR / 'euro.groups.json', encoding=ENCODING) as f:
+with open(BASE_DIR / "euro.groups.json", encoding=ENCODING) as f:
     groups_data = json.load(f)
 
-team_df = pd.read_csv(BASE_DIR / 'teams.csv')
+team_df = pd.read_csv(BASE_DIR / "teams.csv")
 
-group_df = pd.read_csv(BASE_DIR / 'groups.csv')
+group_df = pd.read_csv(BASE_DIR / "groups.csv")
 
 
 def calculate_team_points(points_table: dict) -> dict:
-    return group_df['Tier1'].map(points_table) + group_df['Tier2'].map(points_table) + group_df['Tier3'].map(points_table)
+    return (
+        group_df["Tier1"].map(points_table) + group_df["Tier2"].map(points_table) + group_df["Tier3"].map(points_table)
+    )
 
 
 # Function to calculate points
 def calculate_points(data):
-    points_table = {}
+    points_table = defaultdict(int)
     date_table = {}
 
     for round in data["rounds"]:
         for match in round["matches"]:
             # Ensure 'score' and 'ft' keys exist in the match
-            if 'score' in match and 'ft' in match['score']:
-                score1 = match['score']['ft'][0]
-                score2 = match['score']['ft'][1]
-                team1 = match['team1']["name"]
-                team2 = match['team2']["name"]
-                
-                if team1 not in points_table:
-                    points_table[team1] = 0
-                if team2 not in points_table:
-                    points_table[team2] = 0
+            if "score" in match and "ft" in match["score"]:
+                score1 = match["score"]["ft"][0]
+                score2 = match["score"]["ft"][1]
+                team1 = match["team1"]["name"]
+                team2 = match["team2"]["name"]
 
                 if score1 > score2:
                     points_table[team1] += 3
@@ -52,18 +52,19 @@ def calculate_points(data):
                     points_table[team2] += 1
             else:
                 print(f"Skipping match due to missing 'ft' score: {match}")
-            
+
             date = datetime.strptime(match["date"], "%Y-%m-%d")
             date_table[date] = calculate_team_points(points_table)
 
     return pd.DataFrame(date_table)
 
+
 # Calculate points
 date_df = calculate_points(euro_data)
 
 # Load team config and calc points
-group_df['Total Points'] = date_df[date_df.columns[-1]]
-group_df.sort_values(by='Total Points', ascending=False, inplace=True)
+group_df["Total Points"] = date_df[date_df.columns[-1]]
+group_df.sort_values(by="Total Points", ascending=False, inplace=True)
 
 
 # Display results
@@ -71,8 +72,8 @@ st.title(f"Euro 2024 Points Table {datetime.today().strftime('%d-%m-%Y')}")
 st.dataframe(group_df, hide_index=True)
 
 # Flip and display
-date_df['Group'] = group_df.Group
-date_df.set_index('Group', inplace=True)
+date_df["Group"] = group_df.Group
+date_df.set_index("Group", inplace=True)
 graph_df = date_df.transpose()
 st.subheader("Points over time")
 st.line_chart(graph_df)
